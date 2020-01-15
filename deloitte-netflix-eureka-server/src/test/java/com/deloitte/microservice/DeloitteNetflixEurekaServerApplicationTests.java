0ec09bab817f3e8353151f6c7e334b9a9bb48985
ramanujdas <ramanujds9@gmail.com>
2020-01-15 16:09:09 +0530

    eureka-service

---
+++ deloitte-netflix-eureka-server/src/test/java/com/deloitte/microservice/DeloitteNetflixEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deloitte.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeloitteNetflixEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
